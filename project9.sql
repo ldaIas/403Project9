ba3360446090a9dbab4b790f354de68b5a6d16ef
Max Gawason <maxgawason@Maxs-MacBook-Pro-2.local>
2021-05-04 15:48:49 -0600

    added method to connect to db

---
+++ project9.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS patents;
+DROP TABLE IF EXISTS state_pop;
+DROP TABLE IF EXISTS tax_revenue;
+
+CREATE TABLE state_pop (
+    state CHARACTER(2) PRIMARY KEY,
+    population NUMERIC
+);
+
+CREATE TABLE tax_revenue (
+    state CHARACTER(2) REFERENCES state_pop(state),
+    year TEXT,
+    monthly_revenue NUMERIC,
+    PRIMARY KEY(year, state)
+);
+
+CREATE TABLE patents (
+    year TEXT,
+    state CHARACTER(2) REFERENCES state_pop(state),
+    number_of_patents NUMERIC,
+    PRIMARY KEY(year, state)
+);
+
+\COPY state_pop FROM '/Users/maxgawason/cs403/project9/state_pop_data.csv' DELIMITER ',' CSV HEADER;
+\COPY tax_revenue FROM '/Users/maxgawason/cs403/project9/tax_revenue_data.csv' DELIMITER ',' CSV HEADER;
+\COPY patents FROM '/Users/maxgawason/cs403/project9/patent_data.csv' DELIMITER ',' CSV HEADER;
